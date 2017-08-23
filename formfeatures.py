ca36c3c23ecaebb97066bdb5dac57c1c3ea39395
summersunshine1 <summersunshine366@gmail.com>
2017-08-23 16:37:33 +0800

    fix code bug

--- formfeatures.py
+++ formfeatures.py
@@ -5,30 +5,30 @@ pardir = getparentdir()
 
 user_log_path = pardir+'/data/user_log_format1.csv'
 merchant_path = pardir +'/middledata/merchant.csv'
-train_path = pardir+'/data/test_format1.csv'
-test_path = pardir+'/data/train_format1.csv'
+train_path = pardir+'/data/train_format1.csv'
+test_path = pardir+'/data/test_format1.csv'
 
 train_log_path = pardir+'/data/train_log_format1.csv'
 test_log_path = pardir+'/data/test_log_format1.csv'
 
 def merchantFeature(data):
     merchant = pd.DataFrame()
-    merchant['item_set']=data.groupby("seller_id")['item_id'].apply(set)
+    merchant['item_set']=data.groupby("merchant_id")['item_id'].apply(set)
     merchant['item_num']=(merchant['item_set'].map(len)).astype(np.int16)
     merchant.drop('item_set',1,inplace=True)
-    merchant['cate_set']=data.groupby("seller_id")['cat_id'].apply(set)
+    merchant['cate_set']=data.groupby("merchant_id")['cat_id'].apply(set)
     merchant['cate_num']=(merchant['cate_set'].map(len)).astype(np.int16)
     merchant.drop('cate_set',1,inplace=True)
-    merchant['brand_set']=data.groupby("seller_id")['brand_id'].apply(set)
+    merchant['brand_set']=data.groupby("merchant_id")['brand_id'].apply(set)
     merchant['brand_num']=(merchant['brand_set'].map(len)).astype(np.int16)
     merchant.drop('brand_set',1,inplace=True)
-    group = data.groupby("seller_id")
+    group = data.groupby("merchant_id")
     merchant['click']=group.apply(lambda g:len(g[g['action_type']==0]))
     merchant['add_to_carts'] =group.apply(lambda g:len(g[g['action_type']==1]))
     merchant['purchase']=group.apply(lambda g:len(g[g['action_type']==2]))
     merchant['add_to_favourite'] =group.apply(lambda g:len(g[g['action_type']==3]))
     del group
-    merchant.reset_index(level=['seller_id'],inplace = True)
+    merchant.reset_index(level=['merchant_id'],inplace = True)
     merchant.to_csv(merchant_path,encoding='utf-8',mode = 'w', index = False)
     del merchant
 
@@ -47,14 +47,15 @@ def split_train_test(data):
     train_ = train.groupby(['user_id','merchant_id']).count()
     del train
     train_.reset_index(level=['user_id','merchant_id'],inplace = True)
-    s1 = pd.merge(train_[['user_id','merchant_id']],data,how='inner', on=['user_id','merchant_id'])
+    s1 = pd.merge(train_['user_id','merchant_id'],data,how='inner', on=['user_id','merchant_id'])
     del train_
     s1.to_csv(train_log_path,encoding='utf-8',mode = 'w', index = False)
     del s1
     test= pd.read_csv(test_path,encoding='utf-8')
     test_ = test.groupby(['user_id','merchant_id']).count()
     del test
-    s2 = pd.merge(test_[['user_id','merchant_id']],data,how='inner', on=['user_id','merchant_id'])
+    test_.reset_index(level=['user_id','merchant_id'],inplace = True)
+    s2 = pd.merge(test_['user_id','merchant_id'],data,how='inner', on=['user_id','merchant_id'])
     del test_
     s2.to_csv(test_log_path,encoding='utf-8',mode = 'w', index = False)
     del s2
