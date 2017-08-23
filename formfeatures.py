b691865ffd2950b9cbc69c8cfd214860ce01301d
summersunshine1 <summersunshine366@gmail.com>
2017-08-23 10:28:51 +0800

    add merchant feature

--- formfeatures.py
+++ formfeatures.py
@@ -7,13 +7,21 @@ user_log_path = pardir+'/data/user_log_format1.csv'
 
 def merchantFeature(data):
     merchant = pd.DataFrame()
-    merchant['item_set']=data.groupby(["seller_id")['item_id'].apply(set)
-    merchant['item_num']=(merchant['item_set'].map(len)).as_type(np.int16)
+    merchant['item_set']=data.groupby("seller_id")['item_id'].apply(set)
+    merchant['item_num']=(merchant['item_set'].map(len)).astype(np.int16)
     merchant.drop('item_set',1,inplace=True)
-    merchant['cate_set']=data.groupby(["seller_id")['item_id'].apply(set)
-    merchant['cate_num']=(merchant['item_set'].map(len)).as_type(np.int16)
-    
-    
+    merchant['cate_set']=data.groupby("seller_id")['cat_id'].apply(set)
+    merchant['cate_num']=(merchant['cate_set'].map(len)).astype(np.int16)
+    merchant.drop('cate_set',1,inplace=True)
+    merchant['brand_set']=data.groupby("seller_id")['brand_id'].apply(set)
+    merchant['brand_num']=(merchant['brand_set'].map(len)).astype(np.int16)
+    merchant.drop('brand_set',1,inplace=True)
+    group = data.groupby("seller_id")
+    merchant['click']=group.apply(lambda g:len(g[g['action_type']==0]))
+    merchant['add_to_carts'] =group.apply(lambda g:len(g[g['action_type']==1]))
+    merchant['purchase']=group.apply(lambda g:len(g[g['action_type']==2]))
+    merchant['add_to_favourite'] =group.apply(lambda g:len(g[g['action_type']==3]))
+    del group
     print(merchant)
     
 if __name__=="__main__":
